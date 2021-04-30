547beac98582f98515a863aed99de250984cf028
Ryobia <ryobia36@gmail.com>
2021-04-29 21:31:06 -0600

    minor bug fix with home-routes

--- models/Story.js
+++ models/Story.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes, STRING } = require('sequelize');
-const sequelize = require('../config/connection')
+const sequelize = require('../config/connection');
 
 class Story extends Model {
   static like(body, models) {
