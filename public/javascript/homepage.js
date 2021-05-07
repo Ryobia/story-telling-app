80432335c3f131e5152ceebabd9f89f77974b7eb
Ryobia <ryobia36@gmail.com>
2021-05-06 19:01:49 -0600

    story post works

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -1,30 +1,42 @@
 document.getElementById("display-confirmation").style.display = "none";
 
-async function newStory(event){
-    console.log ("hello");
-    let title = document.querySelector("#title").value.trim();
-    let beginning = document.querySelector("#story-text").value.trim();
+async function newStory(event) {
+  event.preventDefault();
+  console.log("hello");
+  const title = document.querySelector("#title").value.trim();
+  const beginning = document.querySelector('textarea[name="storyText"]').value.trim();
+  console.log(title, beginning);
 
-    const response = await fetch ("/api/stories", {
-        method: "POST",
-        body: JSON.stringify({title, beginning}),
-        headers: {"Content-type": "application/json"}
+  if (title && beginning) {
+    const response = await fetch("/api/stories", {
+      method: "POST",
+      body: JSON.stringify({ 
+          title, 
+          beginning 
+        }),
+      headers: { "Content-type": "application/json" },
     });
 
     if (response.ok) {
-        document.location.replace ("/")
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
     }
-    else {alert (response.statusText)}
+  }
 }
 
-document.getElementById("post-story-btn").addEventListener("click", function () {
-    newStory;
-    document.getElementById("input-fields").style.visibility = "hidden";
-    document.getElementById("display-confirmation").style.display = "inline";
-}); 
+
 
 function goToLogin() {
-    window.location.replace('/login');
+  window.location.replace("/login");
 }
 
-document.querySelector(".profile-button").addEventListener('click', goToLogin);
+document.querySelector(".profile-button").addEventListener("click", goToLogin);
+
+document.querySelector(".story-form").addEventListener("submit", newStory);
+
+// function () {
+//     newStory;
+//     document.getElementById("input-fields").style.visibility = "hidden";
+//     document.getElementById("display-confirmation").style.display = "inline";
+//   });
